8ce551f13db0c76c24d432b083bb28b058ba6b1e
Ashkan Kh. Nazary <ashkan.nazary@mobiliva.com>
2019-08-26 17:56:13 +0300

    Added akka and friends

--- build.sbt
+++ build.sbt
@@ -6,5 +6,7 @@ scalaVersion := "2.13.0"
 
 libraryDependencies ++= Seq(
   "io.spray" %%  "spray-json" % "1.3.5",
+  "com.typesafe.akka" %% "akka-actor" % "2.6.0-M6",
+  "com.typesafe.akka" %% "akka-stream" % "2.6.0-M6",
   "com.typesafe.akka" %% "akka-http"   % "10.1.9"
 )
--- src/main/scala/ir/ashkan/Application.scala
+++ src/main/scala/ir/ashkan/Application.scala
@@ -1,7 +1,12 @@
 package ir.ashkan
 
-import ir.ashkan.Application.input
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.server.Directives._
+import akka.stream.ActorMaterializer
 
+import scala.io.StdIn
 import scala.collection.SortedMap
 
 object Application extends App {
@@ -121,6 +126,26 @@ object Application extends App {
   ]
 }
   """.stripMargin
+
+  implicit val system = ActorSystem("Wolt")
+  implicit val mat = ActorMaterializer()
+  implicit val executionContext = system.dispatcher
+
+  val route =
+    path("hello") {
+      get {
+        complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
+      }
+    }
+
+  val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+
+  println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+  StdIn.readLine() // let it run until user presses return
+  bindingFuture
+    .flatMap(_.unbind()) // trigger unbinding from the port
+    .onComplete(_ => system.terminate()) // and shutdown when done
+
 
   println(api(input))
   println(api(input2))
